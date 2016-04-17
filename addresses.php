7a4619b021113dcae521a1f8f5dd4da1dc158cff
salexseen <salexseen@gmail.com>
2016-04-17 18:29:41 +0300

    fix

--- addresses.php
+++ addresses.php
@@ -1,42 +1,34 @@
-<?
-// ====================================\
-  @include_once('_dir.php');
-// ====================================/
+<?php
+@include_once('_dir.php');
 
-//--------------------------\
-  if (!$SC_QUANTITY) {
+if (!$SC_QUANTITY) {
     redirect('sc.html');
     exit;
-    }
-//--------------------------/
+}
 
-  $Res=_LOAD_DATA("$SC_SITE_SECURE_URL/EXTERNAL/addresses.php?GET_XML=0&SHOPXML_SESSION=$SHOPXML_SESSION&new_addresses_url=".to_url('addresses_process.html').translate_to_url(),'POST');
-  $Error=get_error($Res);
+$Res = _LOAD_DATA("$SC_SITE_SECURE_URL/EXTERNAL/addresses.php?GET_XML=0&SHOPXML_SESSION=$SHOPXML_SESSION&new_addresses_url=" . to_url('addresses_process.html') . translate_to_url(), 'POST');
+$Error = get_error($Res);
 
-  include_once("$ROOT_PATH/common/all_head.php");
+include_once("$ROOT_PATH/common/all_head.php");
 ?>
 
-<center>
+    <div class="checkout">
 
-<?
-//-----------------------\
-report_error($error);
-//-----------------------/
-?>
+        <?php
+        report_error($error);
+        ?>
 
-<?
-//-------------------------------\
-if ($Error) report_error($Error);
-else {
-  $FromDisc=1;
-  include('modules/sc_info.php');
-  echo '<br>',$Res;
-  }
-//-------------------------------/
-?>
+        <?php
+        if ($Error) report_error($Error);
+        else {
+            $FromDisc = 1;
+            include('modules/sc_info.php');
+            echo '<br>', $Res;
+        }
+        ?>
 
-</center>
+    </div>
 
-<?
-  include_once("$ROOT_PATH/common/all_tail.php");
+<?php
+include_once("$ROOT_PATH/common/all_tail.php");
 ?>
