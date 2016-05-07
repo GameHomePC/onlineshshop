65a92a56c4c90df4c8ada7e4568e0d6a7c1924e6
salexseen <salexseen@gmail.com>
2016-05-07 18:37:30 +0300

    fix

--- news.php
+++ news.php
@@ -1,56 +1,65 @@
-<?
+<?php
 // ====================================\
-  @include_once('_dir.php');
+@include_once('_dir.php');
 // ====================================/
 
 // ------------------------------------\
-  $page=(int)$args[0];
+$page = (int)$args[0];
 // ------------------------------------/
 
-  include_once("$ROOT_PATH/common/all_head.php");
+include_once("$ROOT_PATH/common/all_head.php");
 ?>
 
 
 
-<?
+<?php
 // ----------------------------------------------------------------------------\
-  $portion=(int)$Config['num_news'];
-  $num_rows=@$sql_result(db_query("select count(*) from news"),0,0);
-  $page=(int)$page;
+$portion = (int)$Config['num_news'];
+$num_rows = @$sql_result(db_query("select count(*) from news"), 0, 0);
+$page = (int)$page;
 
-  list($hbar,$lbar,$page)=make_page_bar("news_",$num_rows,$page,1,0,$portion);
-  $lbar=preg_replace('/news_\d+/','\0.html',$lbar);
+list($hbar, $lbar, $page) = make_page_bar("news_", $num_rows, $page, 1, 0, $portion);
+$lbar = preg_replace('/news_\d+/', '\0.html', $lbar);
 
-  $start=$page*$portion;
+$start = $page * $portion;
 
-  $res=db_query("select nwsID,time,title,content,source
+$res = db_query("select nwsID,time,title,content,source
 		from news order by time DESC,nwsID DESC limit $start,$portion");
 // ----------------------------------------------------------------------------/
 ?>
 
 
-<?= $hbar ?>
-<div class='txt'><?= $lbar ?></div>
+<?php echo $hbar; ?>
+<div class="newsShort">
+    <?php
+        $ml = 150;
+        while ($row = @$sql_fetch_assoc($res)) {
+            @extract($row);
+            $time = get_date_str($time);
+            $title = to_html($title);
+            $content = strip_tags(str_replace('<%BASE%>', $SITE_ROOT, $content));
+            if (strlen($content) > $ml) $content = substr($content, 0, $ml) . '...';
+            echo "
+                <div id='$nwsID' class='newsShort__item'>
+                    <div class='newsShort__title'>
+                        <a href='news_view_$nwsID.html'>[$time] $title</a>
+                    </div>
+                    <div class='newsShort__content'>
+                        $content
+                    </div>
+                </div>
+            ";
+        }
+    ?>
+</div>
 
-<br>
-<?
-  $ml=150;
-  while ($row=@$sql_fetch_assoc($res)) {
-    @extract($row);
-    $time=get_date_str($time);
-    $title=to_html($title);
-    $content=strip_tags(str_replace('<%BASE%>',$SITE_ROOT,$content));
-    if (strlen($content)>$ml) $content=substr($content,0,$ml).'...';
-    echo "<a name='$nwsID'></a>
-	<div class=head><a href='news_view_$nwsID.html'>[$time] $title</a></div>
-	$content<br><br>";
-    }
-?>
+<div class='newsShortNav'>
+    <div class="newsShortNav__content">
+        <?php echo $lbar; ?>
+    </div>
+</div>
+<?php echo $hbar; ?>
 
-<div class='txt'><?= $lbar ?></div>
-<?= $hbar ?>
-
-
-<?
-  include_once("$ROOT_PATH/common/all_tail.php");
+<?php
+    include_once("$ROOT_PATH/common/all_tail.php");
 ?>
